816dc3c9427cd2653e01e8e5d80d36eb031e2859
abid211051 <apranto41@gmail.com>
2025-02-03 12:07:05 +0600

    changed

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://cultivisusers:<db_password>@userscluster.ly6q4.mongodb.net/aytfurniture?retryWrites=true&w=majority&appName=userscluster`;
-
-const client = new MongoClient(uri, {
+const client = new MongoClient(process.env.URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -23,63 +21,85 @@ const reviewcollection = client.db("aytfurniture").collection("reviews");
 const contactcollection = client.db("aytfurniture").collection("contacts");
 
 app.post("/add-a-product", async (req, res) => {
-  const furniture = req.body;
-  const result = await furniturecollection.insertOne(furniture);
-  res.send(result);
+  try {
+    const furniture = req.body;
+    const result = await furniturecollection.insertOne(furniture);
+    res.send(result);
+  } catch (error) {
+    res.send("Error");
+  }
 });
 
 // Reading All data from a collection
 app.get("/all-product", async (req, res) => {
-  const result = await furniturecollection.find().toArray();
-  console.log(result);
-
-  res.send(result);
+  try {
+    const result = await furniturecollection.find().toArray();
+    res.send(result);
+  } catch (error) {
+    res.send("Error");
+  }
 });
 
 // Reading single data from a collection
 app.get("/single-product/:id", async (req, res) => {
-  const id = req.params.id;
-  const result = await furniturecollection.findOne({ _id: new ObjectId(id) });
-  res.send(result);
+  try {
+    const id = req.params.id;
+    const result = await furniturecollection.findOne({ _id: new ObjectId(id) });
+    res.send(result);
+  } catch (error) {
+    res.send("Error");
+  }
 });
 
 // Updating single info of from a collection
 app.put("/update-product/:id", async (req, res) => {
-  const id = req.params.id;
-  const filter = { _id: new ObjectId(id) };
-  const updatedfurniture = req.body;
-  const updates = { $set: updatedfurniture };
-  const result = await furniturecollection.updateOne(filter, updates);
-  res.send(result);
+  try {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+
+    const updatedfurniture = req.body;
+
+    const updates = { $set: updatedfurniture };
+    const result = await furniturecollection.updateOne(filter, updates);
+    res.send(result);
+  } catch (error) {
+    res.send("Error");
+  }
 });
 
 // Deleting a data from db
 app.delete("/delete-product/:id", async (req, res) => {
-  const id = req.params.id;
-  const filter = { _id: new ObjectId(id) };
-  const result = await furniturecollection.deleteOne(filter);
-  res.send(result);
+  try {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    const result = await furniturecollection.deleteOne(filter);
+    res.send(result);
+  } catch (error) {
+    res.send("Error");
+  }
 });
 
 // For reading all Review Collection
 app.get("/review", async (req, res) => {
-  const result = await reviewcollection.find().toArray();
-  res.send(result);
+  try {
+    const result = await reviewcollection.find().toArray();
+    res.send(result);
+  } catch (error) {
+    res.send("Error");
+  }
 });
 
 // For creating new feedback from contact
 app.post("/add-contact-info", async (req, res) => {
-  const info = req.body;
-  const result = await contactcollection.insertOne(info);
-  res.send(result);
-});
-
-const data = require("./data.json");
-
-app.get("/", (req, res) => {
-  res.send(data);
+  try {
+    const info = req.body;
+    const result = await contactcollection.insertOne(info);
+    res.send(result);
+  } catch (error) {
+    res.send("Error");
+  }
 });
 
 app.listen(PORT, () => {
-  console.log("Hello AYT! Your server is on");
+  console.log("Server running at PORT: ", PORT);
 });
